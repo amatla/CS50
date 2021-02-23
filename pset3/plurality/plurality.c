0b7cedae7f19a4c4a4c0524d843e3286f5db8e9a
amatla <amatlaow@gmail.com>
2021-02-23 18:55:33 +0100

    Implement vote function

--- pset3/plurality/plurality.c
+++ pset3/plurality/plurality.c
@@ -66,7 +66,14 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
