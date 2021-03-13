99a6c9320d7edabb86ae5d9cb0679df218d04327
amatla <amatlaow@gmail.com>
2021-03-13 16:48:01 +0100

    implement basic input checking

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
-{
- 
+{ 
+  if (argc != 2)
+  {
+    printf("Usage: ./recover image\n");
+    return 1;
+  }
+  FILE *infile = fopen(argv[1], "r");
+
+  if (infile == NULL)
+  {
+    printf("Could not open %s\n", argv[1]);
+    return 1;
+  }
 }
